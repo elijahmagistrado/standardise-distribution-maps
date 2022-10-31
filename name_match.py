ecbdd947194861cf355a5c8eecc4c0906037615a
Elijah Magistrado <elijah.magi28@gmail.com>
2022-10-31 14:29:46 +1100

    added detail to name_match record removal

--- name_match.py
+++ name_match.py
@@ -14,6 +14,8 @@ def name_match(merged, sci_name):
 
     with ThreadPoolExecutor() as executor:
         good_matches = []
+        unsuccessful = []
+        bad_matches = []
         futures = {executor.submit(make_request, name): name for name in name_list}
         for future in as_completed(futures):
             result = future.result()
@@ -23,11 +25,17 @@ def name_match(merged, sci_name):
                     if data['rank'] == 'species' or data['rank'] == 'subspecies':
                         good_matches.append(name)
                         family_list.append(data['family'].upper())
+                else:
+                    bad_matches.append(name)
+            else:
+                unsuccessful.append(name)
 
     if len(merged) != len(good_matches):
         valid_records = merged[merged[f'{sci_name}'].isin(good_matches)].reset_index(drop=True)
         invalid_records = merged[~merged[f'{sci_name}'].isin(good_matches)].reset_index(drop=True)
-        print(f'{len(invalid_records)} low quality record(s) removed')
+        print(f'{len(invalid_records)} low quality record(s) removed.'
+              f'{len(unsuccessful)} species not found in the ALA.'
+              f'{len(bad_matches)} bad matches with ALA taxonomy.')
         valid_records = valid_records
     else:
         valid_records = merged
