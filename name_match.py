857e556564d9ae0b75db35ed0a13f52702cddd1b
Elijah Magistrado <elijah.magi28@gmail.com>
2022-10-31 14:35:39 +1100

    print errors in different lines

--- name_match.py
+++ name_match.py
@@ -16,6 +16,7 @@ def name_match(merged, sci_name):
         good_matches = []
         unsuccessful = []
         bad_matches = []
+        bad_level = []
         futures = {executor.submit(make_request, name): name for name in name_list}
         for future in as_completed(futures):
             result = future.result()
@@ -25,6 +26,8 @@ def name_match(merged, sci_name):
                     if data['rank'] == 'species' or data['rank'] == 'subspecies':
                         good_matches.append(name)
                         family_list.append(data['family'].upper())
+                    else:
+                        bad_level.append(name)
                 else:
                     bad_matches.append(name)
             else:
@@ -33,9 +36,10 @@ def name_match(merged, sci_name):
     if len(merged) != len(good_matches):
         valid_records = merged[merged[f'{sci_name}'].isin(good_matches)].reset_index(drop=True)
         invalid_records = merged[~merged[f'{sci_name}'].isin(good_matches)].reset_index(drop=True)
-        print(f'{len(invalid_records)} low quality record(s) removed.'
-              f'{len(unsuccessful)} species not found in the ALA.'
-              f'{len(bad_matches)} bad matches with ALA taxonomy.')
+        print(f'{len(invalid_records)} low quality record(s) removed.')
+        print(f'{len(unsuccessful)} species not found in the ALA.')
+        print(f'{len(bad_matches)} bad matches with ALA taxonomy.')
+        print(f'{len(bad_level)} record(s) not down to species or subspecies level.')
         valid_records = valid_records
     else:
         valid_records = merged
