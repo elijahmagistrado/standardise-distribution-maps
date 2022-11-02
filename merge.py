ff0879d4297e839d074e3551e0fa48f0465177e9
Elijah Magistrado <elijah.magi28@gmail.com>
2022-11-02 11:50:06 +1100

    now unites small polygons to multipolygons before merging

--- merge.py
+++ merge.py
@@ -13,17 +13,19 @@ def merge(file_path, sci_name, converted=bool, dist_type=None, current=None):
             if file.endswith('.shp'):
                 sf = gpd.read_file(os.path.join(root, file))
 
+                if converted:
+                    sf = sf.dissolve(by=sci_name, as_index=False)
+                    sf[sci_name] = sf[sci_name].replace(1, file.split('_currentF.shp')[0].replace('_', ' '))
+
                 if dist_type is not None:
                     sf = sf.loc[sf[f'{dist_type}'] == f'{current}']
                 sf = sf.to_crs(epsg=4326)  # Reproject to WGS84
+
                 merged = pd.concat([merged, sf])
 
-                if converted:
-                    merged[sci_name] = merged[sci_name].replace(1, file.split('_currentF.shp')[0].replace('_', ' '))
+
 
     # Grouping polygons of the same taxon to a multipolygon
     merged = merged.dissolve(by=sci_name, as_index=False)
 
     return merged
-
-b = merge('F:\GISFiles\\test', 'species', True)
