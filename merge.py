b35a0814f04591f26f842d37da8e5056d765b7a8
Elijah Magistrado <elijah.magi28@gmail.com>
2022-11-10 16:33:31 +1100

    added function to remove small holes and polygon parts

--- merge.py
+++ merge.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import pandas as pd
 import os
+from shapely.geometry import Polygon
 
 
 # Combines multiple shapefiles into one
@@ -14,6 +15,8 @@ def merge(file_path, sci_name, converted=bool, dist_type=None, current=None):
                 sf = gpd.read_file(os.path.join(root, file))
 
                 if converted:
+
+                    sf = remove_small_parts(sf, sci_name)
                     sf = sf.dissolve(by=sci_name, as_index=False)
                     sf[sci_name] = sf[sci_name].replace(1, file.split('_currentF.shp')[0].replace('_', ' '))
 
@@ -23,9 +26,35 @@ def merge(file_path, sci_name, converted=bool, dist_type=None, current=None):
 
                 merged = pd.concat([merged, sf])
 
-
-
     # Grouping polygons of the same taxon to a multipolygon
     merged = merged.dissolve(by=sci_name, as_index=False)
 
     return merged
+
+
+def remove_small_parts(polygon, sci_name):
+    list_parts = []
+
+    for polygon in polygon.geometry:
+        list_interiors = []
+        for interior in polygon.interiors:
+            p = Polygon(interior)
+            if p.area > 0.003:
+                list_interiors.append(interior)
+
+        temp_pol = Polygon(polygon.exterior.coords, holes=list_interiors)
+        list_parts.append(temp_pol)
+
+    new_mp = gpd.GeoSeries(list_parts)
+
+    d = []
+
+    for polygon in new_mp:
+        if polygon.area > 0.006:
+            d.append(polygon)
+
+    cleaned = gpd.GeoDataFrame(columns=[sci_name], geometry=d, crs='EPSG:4326')
+    cleaned[sci_name] = 1
+
+    return cleaned
+
