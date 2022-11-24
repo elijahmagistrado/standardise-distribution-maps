325307ee421b04f1b08a9dc63c13a04087669773
Elijah Magistrado <elijah.magi28@gmail.com>
2022-11-24 19:59:49 +1100

    changed http request from threading to async

--- standardise/preprocess/shp_name_match.py
+++ standardise/preprocess/shp_name_match.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import requests
+import asyncio
+import httpx
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
@@ -7,40 +9,41 @@ def name_match(merged, sci_name):
     # Name matching records with API
     api_link = "https://namematching-ws.ala.org.au/api/searchByClassification"
     name_list = merged[sci_name].tolist()      # List of species names to submit to the API
+    tasks = []
 
-    # Function used for sending API request
-    def make_request(name):
-        with requests.get(api_link, params={'scientificName': name}) as match:
-            return match.json(), name
+    # Asynchronously sending GET request
+    async def submit_request():
+        async with httpx.AsyncClient() as client:
+            for name in name_list:
+                task = client.get(api_link, params={'scientificName': name})
+                tasks.append(task)
+            responses = await asyncio.gather(*tasks)
+        return name, responses
 
-    # Submitting multiple requests simultaneously using threading
-    with ThreadPoolExecutor() as executor:
-        good_matches = []
-        not_found = []              # Species not found in the ALA taxonomy
-        bad_matches = []            # Not exact or canonical matches
-        insufficient_level = []     # Not down to species or subspecies level
+    sp_name, response_list = asyncio.run(submit_request())
 
-        matched_list = pd.DataFrame()
+    good_matches = []
+    not_found = []              # Species not found in the ALA taxonomy
+    bad_matches = []            # Not exact or canonical matches
+    insufficient_level = []      # Not down to species or subspecies level
+    matched_list = pd.DataFrame()
 
-        futures = {executor.submit(make_request, name): name for name in name_list}
-        for future in as_completed(futures):
-            result = future.result()
-            data, name = result
-
-            # Filtering and classifying results based on their validity
-            if data['success']:
-                if data['matchType'] in ['exactMatch', 'canonicalMatch']:
-                    if data['rank'] in ['species', 'subspecies']:
-                        if 'family' in data:
-                            good_matches.append(name)
-                            data_df = pd.DataFrame({"species": [name], "family": [data['family']]})
-                            matched_list = pd.concat([matched_list, data_df])
-                    else:
-                        insufficient_level.append(name)
+    # Filtering and classifying results based on their validity
+    for response in response_list:
+        data = response.json()
+        if data['success']:
+            if data['matchType'] in ['exactMatch', 'canonicalMatch']:
+                if data['rank'] in ['species', 'subspecies']:
+                    if 'family' in data:
+                        good_matches.append(sp_name)
+                        data_df = pd.DataFrame({"species": [sp_name], "family": [data['family']]})
+                        matched_list = pd.concat([matched_list, data_df])
                 else:
-                    bad_matches.append(name)
+                    insufficient_level.append(sp_name)
             else:
-                not_found.append(name)
+                bad_matches.append(sp_name)
+        else:
+            not_found.append(sp_name)
 
     # Filtering invalid records from merged GeoDataFrame
     if len(merged) != len(good_matches):
