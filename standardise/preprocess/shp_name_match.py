97a64bab62385891f48f9f5fbe1468ad2157f6d9
Elijah Magistrado <elijah.magi28@gmail.com>
2022-11-25 14:52:47 +1100

    async name_match now functioning properly

--- standardise/preprocess/shp_name_match.py
+++ standardise/preprocess/shp_name_match.py
@@ -1,42 +1,48 @@
 import pandas as pd
-import requests
 import asyncio
-import httpx
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import aiohttp
 
 
 def name_match(merged, sci_name):
     # Name matching records with API
     api_link = "https://namematching-ws.ala.org.au/api/searchByClassification"
-    name_list = merged[sci_name].tolist()      # List of species names to submit to the API
-    tasks = []
 
-    # Asynchronously sending GET request
-    async def submit_request():
-        async with httpx.AsyncClient() as client:
-            for name in name_list:
-                task = client.get(api_link, params={'scientificName': name})
-                tasks.append(task)
-            responses = await asyncio.gather(*tasks)
-        return name, responses
+    async def submit_request(session, name):
+        async with session.get(api_link, params={'scientificName': name}) as req:
+            return name, await req.json()
 
-    sp_name, response_list = asyncio.run(submit_request())
+    async def submit_all_requests(session, name_list):
+        tasks = []
+        for name in name_list:
+            task = asyncio.create_task(submit_request(session, name))
+            tasks.append(task)
+        res = await asyncio.gather(*tasks)
+        return res
 
-    good_matches = []
-    not_found = []              # Species not found in the ALA taxonomy
-    bad_matches = []            # Not exact or canonical matches
-    insufficient_level = []      # Not down to species or subspecies level
+    async def send_names():
+        name_list = merged[sci_name].tolist()
+
+        async with aiohttp.ClientSession() as session:
+            data = await submit_all_requests(session, name_list)
+            return data
+
+    api_data = asyncio.run(send_names())
+
     matched_list = pd.DataFrame()
+    good_matches = []
+    insufficient_level = []
+    bad_matches = []
+    not_found = []
 
-    # Filtering and classifying results based on their validity
-    for response in response_list:
-        data = response.json()
-        if data['success']:
-            if data['matchType'] in ['exactMatch', 'canonicalMatch']:
-                if data['rank'] in ['species', 'subspecies']:
-                    if 'family' in data:
+    for item in api_data:
+        sp_name, sp_info = item
+
+        if sp_info['success']:
+            if sp_info['matchType'] in ['exactMatch', 'canonicalMatch']:
+                if sp_info['rank'] in ['species', 'subspecies']:
+                    if 'family' in sp_info:
                         good_matches.append(sp_name)
-                        data_df = pd.DataFrame({"species": [sp_name], "family": [data['family']]})
+                        data_df = pd.DataFrame({"species": [sp_name], "family": [sp_info['family']]})
                         matched_list = pd.concat([matched_list, data_df])
                 else:
                     insufficient_level.append(sp_name)
